1553e6facab87ded4c3cdd84fea61541361be4dc
Mariah Luisa <sophiafagleal@gmail.com>
2023-11-10 16:52:07 -0300

    segundo codigo php

---
+++ exemplo.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+
+<body>
+    <h1>PHP!!!!!!!!!!!!!<h1>
+            <h2> EU não sei o que estpou fazendo!!!!!!!!!!!!!! <h2>
+<?php
+    echo "<p style='color:red;'>Aqui ja e php!<p>";
+    echo "<button> Enviar </button>";
+    echo "<button>" . $texto . "</button>";
+    ?>
+</body>
+
+</html>
