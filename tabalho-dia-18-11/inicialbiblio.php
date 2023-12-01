9b434d769170f4399f707cb58f563c71ac9c02aa
sophia <sophiafagleal@gmail.com>
2023-12-01 17:27:17 -0300

    versão 1.5 do site do vendedor

--- tabalho-dia-18-11/inicialbiblio.php
+++ tabalho-dia-18-11/inicialbiblio.php
@@ -12,13 +12,20 @@
     <header>
 
         <p class="menu1">
-
+            <a href="https://blog.cod3r.com.br/guia-basico-de-git-como-fazer-commit-no-github/">Consultar livros</a>
+            <p>|</p>
+            <a href="">Pedidos Online</a>
+            <p>|</p>
+            <a href="">Estoque</a>
+            <p>|</p>
+            <a href="">Loja virtual</a>
         </p>
 
         <p class="menu2">
-
+            <a><img src="">Usurario</a>
+            <a><img src=""> Abrir chamado</a>
         </p>
-        
+
     </header>
 
     <section>
