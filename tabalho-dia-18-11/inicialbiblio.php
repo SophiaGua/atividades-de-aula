0104dc8c278e192588d8b5a2fbd8825890d86af2
sophia <sophiafagleal@gmail.com>
2023-12-03 20:05:27 -0300

    modificações de layout

--- tabalho-dia-18-11/inicialbiblio.php
+++ tabalho-dia-18-11/inicialbiblio.php
@@ -4,6 +4,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="./src/css/style.css">
     <title>Document</title>
 </head>
 
@@ -13,17 +14,17 @@
 
         <p class="menu1">
             <a href="https://blog.cod3r.com.br/guia-basico-de-git-como-fazer-commit-no-github/">Consultar livros</a>
-            <p>|</p>
+
             <a href="">Pedidos Online</a>
-            <p>|</p>
+         
             <a href="">Estoque</a>
-            <p>|</p>
+           
             <a href="">Loja virtual</a>
         </p>
 
         <p class="menu2">
-            <a><img src="">Usurario</a>
-            <a><img src=""> Abrir chamado</a>
+            <a href=""><img src="">Usurario</a>
+            <a href=""><img src=""> Abrir chamado</a>
         </p>
 
     </header>
